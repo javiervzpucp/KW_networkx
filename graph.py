c91b7f7ecb46294c3e2a7cc0b66237e857e7592a
javiervzpucp <jveraz@pucp.edu.pe>
2024-12-03 16:54:08 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -6,10 +6,8 @@ Created on Mon Nov 11 16:34:42 2024
 """
 
 import os
-from langchain_community.graphs import Neo4jGraph
 from langchain_openai import ChatOpenAI
 from langchain_experimental.graph_transformers import LLMGraphTransformer
-from neo4j import GraphDatabase
 from langchain_core.documents import Document
 import networkx as nx
 import json
@@ -23,30 +21,6 @@ load_dotenv()
 
 # Set the OpenAI API key from the .env file
 api_key = os.getenv("OPENAI_API_KEY")
-url = os.getenv("NEO4J_URI")
-neo_user = os.getenv("NEO4J_USERNAME")
-neo_pass = os.getenv("NEO4J_PASSWORD")
-
-# Borrar datos del grafo existente en Neo4j
-driver = GraphDatabase.driver(url, auth=(neo_user, neo_pass))
-
-# Función para borrar el grafo completo
-def clear_graph(tx):
-    tx.run("MATCH (n) DETACH DELETE n")
-
-# Ejecutar la función para borrar el grafo
-with driver.session() as session:
-    session.write_transaction(clear_graph)
-
-# Cierra la conexión
-driver.close()
-
-# Configurar la conexión con Neo4j utilizando las credenciales cargadas
-graph = Neo4jGraph(
-    url=url,
-    username=neo_user,
-    password=neo_pass
-)
 
 # Función para leer todos los archivos .txt, .pdf y .docx de la carpeta "archivos"
 def read_files_from_folder(folder_path):
@@ -88,11 +62,6 @@ for i, chunk in enumerate(text_chunks):
     llm_transformer = LLMGraphTransformer(llm=llm)
 
     graph_documents = llm_transformer.convert_to_graph_documents([document])
-    graph.add_graph_documents(
-        graph_documents,
-        baseEntityLabel=True,
-        include_source=False
-    )
     
     # Agregar nodos y aristas del fragmento al grafo
     for doc in graph_documents:
@@ -107,52 +76,10 @@ for i, chunk in enumerate(text_chunks):
             target = str(dict(edge_dict["target"])["id"])
             tipo = edge_dict["type"]
             nx_graph.add_edge(source, target, content=tipo)
-
-# Configura la conexión a Neo4j
-uri = url  # Cambia esto según la URI de tu instancia de Neo4j
-user = neo_user  # Cambia esto a tu usuario
-password = neo_pass  # Cambia esto a tu contraseña
-driver = GraphDatabase.driver(uri, auth=(user, password))
-
-# Función para extraer nodos y relaciones
-def get_graph_data(tx):
-    nodes = []
-    relationships = []
-    
-    # Obtener nodos
-    for record in tx.run("MATCH (n) RETURN n"):
-        node = record["n"]
-        nodes.append({
-            "id": node.id,
-            "labels": list(node.labels),
-            "properties": dict(node)
-        })
-    
-    # Obtener relaciones
-    for record in tx.run("MATCH (a)-[r]->(b) RETURN a, b, r"):
-        relationship = record["r"]
-        relationships.append({
-            "source": record["a"].id,
-            "target": record["b"].id,
-            "type": relationship.type,
-            "properties": dict(relationship)
-        })
-    
-    return {"nodes": nodes, "edges": relationships}
-
-# Extrae los datos del grafo y guárdalos en un archivo JSON
-with driver.session() as session:
-    graph_data = session.read_transaction(get_graph_data)
-
-with open('paucar_graph.json', 'w') as f:
-    json.dump(graph_data, f, indent=4)
-
-# Cierra la conexión a Neo4j
-driver.close()
             
 # Guardar el grafo en JSON
 data = nx.node_link_data(nx_graph)
-with open("saved_graph.json", "w") as file:
+with open("graph.json", "w") as file:
     json.dump(data, file)
     
 # Procesar el componente más grande del grafo
